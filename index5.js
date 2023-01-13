6a91e9732418bd64096a2812083184aa91c07ff9
Abdul Moiz Anwer <89318559+AbdulMoizAnwer@users.noreply.github.com>
2023-01-13 22:29:00 +0500

    Create index5.js

---
+++ index5.js
@@ -0,0 +1,17 @@
+// Write a JavaScript Program to print table for given number (8, 9, 12, 15) using for Loop?
+
+for (var num = 1; num <= 10; num++){
+    console.log(8 + " x " + num + " = " + (8 * num));
+}
+console.log(" ");
+for (var num = 1; num <= 10; num++){
+    console.log(9 + " x " + num + " = " + (9 * num));
+}
+console.log(" ");
+for (var num = 1; num <= 10; num++){
+    console.log(12 + " x " + num + " = " + (12 * num));
+}
+console.log(" ");
+for (var num = 1; num <= 10; num++){
+    console.log(15 + " x " + num + " = " + (15 * num));
+}
